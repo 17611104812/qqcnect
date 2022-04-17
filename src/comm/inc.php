90c0828a3dedb57cf7683fcacebbcb6fb4ffd67d
吕向杰 <v-xiangjielv@creditease.cn>
2022-04-17 12:06:31 +0800

    初始化

---
+++ src/comm/inc.php
@@ -0,0 +1,2 @@
+<?php die('forbidden'); ?>
+{"appid":"appid","appkey":"appkey","callback":"example/oauth/callback.php","scope":"get_user_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
